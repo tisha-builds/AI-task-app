b066cd9cb9b4bc6142dbb8ab6e8732f5be353cdb
Tishajain122004 <you@example.com>
2026-05-06 20:34:26 +0530

    fix ci pipeline

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  plugins: ['@typescript-eslint'],
+
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+}
